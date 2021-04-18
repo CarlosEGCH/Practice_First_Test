fc819f6f18c3f41d586e128cb39a6a54e4ab4e8d
Carlos Gomes <79974236+EUWFahrenHate@users.noreply.github.com>
2021-04-18 17:24:16 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Practice_First_Test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practice_First_Test.dir/main.cpp.obj"
+  "Practice_First_Test.exe"
+  "Practice_First_Test.exe.manifest"
+  "Practice_First_Test.lib"
+  "Practice_First_Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practice_First_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Practice_First_Test.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Practice_First_Test.
+# This may be replaced when dependencies are built.
